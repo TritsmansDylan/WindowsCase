c31184f0ede0ccad204e565dbe36609fb7e45068
TritsmansDylan <r0804341@student.thomasmore.be>
2020-12-03 18:28:59 +0100

    voorbeeld demo

--- WSE case study/Program.cs
+++ WSE case study/Program.cs
@@ -9,6 +9,7 @@ namespace WSE_case_study
             Console.WriteLine("Hello World!");
             Console.WriteLine("first push?");
             Console.WriteLine("YES IT WORKS");
+            Console.WriteLine("voorbeeld");
             Console.ReadLine();
         }
     }

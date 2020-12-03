80dbaedc77019bcc90ec54f58c634fe8883b9b18
TritsmansDylan <r0804341@student.thomasmore.be>
2020-12-03 12:58:34 +0100

    helloworld

---
+++ WSE case study/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSE_case_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
